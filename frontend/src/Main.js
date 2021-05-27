f19de9233aed0188a248647d91c5d189d213e7dc
Davis Owen <sdowen12@gmail.com>
2021-05-26 22:36:52 -0700

    changed the title a bit

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
   const dispatch = useDispatch();
   return (
     <div className="container mt-3">
-      <h1>React Cookie Auth</h1>
+      <h1>Quant.io</h1>
       <p>You are logged in!</p>
       <button className="btn btn-danger" onClick={() => dispatch(logoutThunk)}>Log out</button>
     </div>
